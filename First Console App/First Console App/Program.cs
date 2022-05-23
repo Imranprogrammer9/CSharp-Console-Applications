549a308ff3f10f4b8ab84196f11aa6ed32ce2226
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-05-23 23:23:08 +0500

    First C# Console Application

---
+++ First Console App/First Console App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------------------------Welcome to C# Programming!----------------------------------------");
+            Console.WriteLine("Enter Your First Name:");
+            String FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Your Last Name:");
+            String LastName = Console.ReadLine();
+            Console.WriteLine("Enter Your Education:");
+            String Education = Console.ReadLine();
+            Console.WriteLine("Enter Your Skills");
+            String Skill = Console.ReadLine();
+            Console.WriteLine("Enter Your Phone No.");
+            String Phone = Console.ReadLine();
+            Console.WriteLine("Your Profile is Ready. You can check it below!");
+            Console.WriteLine(FirstName + " " + LastName);
+            Console.WriteLine(Education);
+            Console.WriteLine(Skill);
+            Console.WriteLine(Phone);
+            Console.WriteLine("My name is: " + FirstName + " " + LastName + "." + " I have degree of " + Education + "." + 
+                " My SkillSet consists of " + Skill + "." + " Want to work with me! Just contact me:" + Phone + ".");
+            Console.ReadLine();
+        }
+    }
+}
